8cc32ab5382277791eed404aa161a9e8dda631f2
배민수 <duecost@gmail.com>
2024-09-14 16:08:41 +0900

    프로젝트 초기 설정

---
+++ src/test/java/team18/team18_be/Team18BeApplicationTests.java
@@ -0,0 +1,13 @@
+package team18.team18_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team18BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
