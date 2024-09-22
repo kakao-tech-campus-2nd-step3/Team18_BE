4e70bc1ef7404c7e900df1a5e77057a0e096d450
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-09-22 11:23:54 +0900

    feat : controller에 swagger 적용

--- src/main/java/team18/team18_be/resume/controller/ResumeController.java
+++ src/main/java/team18/team18_be/resume/controller/ResumeController.java
@@ -1,13 +1,17 @@
 package team18.team18_be.resume.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team18.team18_be.resume.dto.ResumeRequest;
 import team18.team18_be.resume.dto.ResumeResponse;
 
+@Api(tags = {"이력서 관련 Controller"})
 @RestController
 @RequestMapping("/api/resumes")
 public class ResumeController {
+    @ApiOperation(value = "이력서 저장 메서드")
     @PostMapping
     public ResponseEntity<Void> saveResume(
             @RequestHeader("Authorization") String token,
@@ -16,6 +20,7 @@ public class ResumeController {
         return null;
     }
 
+    @ApiOperation(value = "구직자 별 이력서 조회 메서드")
     @GetMapping
     public ResponseEntity<ResumeResponse> getResume(
             @RequestHeader("Authorization") String token
@@ -23,6 +28,7 @@ public class ResumeController {
         return null;
     }
 
+    @ApiOperation(value = "이력서 id로 이력서 조회 메서드")
     @GetMapping("/resumeId")
     public ResponseEntity<ResumeResponse> getResumeById(
             @RequestHeader("Authorization") String token
