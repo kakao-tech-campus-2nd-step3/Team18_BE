5ea0a6c66e03b49b5d097366ad4437a9317ffbe4
배민수 <duecost@gmail.com>
2024-09-24 21:42:12 +0900

    feat: User 엔티티 구현
    - getter와 생성자는 추후 필요가 생길 때 필요한 것만 추가해주는 식으로
      계획 중

---
+++ src/main/java/team18/team18_be/auth/entity/User.java
@@ -0,0 +1,60 @@
+package team18.team18_be.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id;
+
+    @Column(nullable = false)
+    private final String name;
+
+    @Column(nullable = false)
+    private final String email;
+
+    private final String phoneNumber;
+
+    private final String foreignerNumber;
+
+    private final LocalDateTime visaGenerateDate;
+
+    private final String type;
+
+    private final Long signId;
+
+    private final Long resumeId;
+
+    protected User() {
+        this(null, null, null, null, null, null, null, null, null);
+    }
+
+    public User(Long id,
+        String name,
+        String email,
+        String phoneNumber,
+        String foreignerNumber,
+        LocalDateTime visaGenerateDate,
+        String type,
+        Long signId,
+        Long resumeId) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.foreignerNumber = foreignerNumber;
+        this.visaGenerateDate = visaGenerateDate;
+        this.type = type;
+        this.signId = signId;
+        this.resumeId = resumeId;
+    }
+
+}
