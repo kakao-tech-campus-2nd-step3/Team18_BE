ebab6f731f7400369271ca50c68dfa99b100a018
test1 <test>
2024-09-26 22:48:34 +0900

    refactor : indent 수정 후 코드정렬

--- src/main/java/team18/team18_be/userInformation/controller/UserInformationController.java
+++ src/main/java/team18/team18_be/userInformation/controller/UserInformationController.java
@@ -1,12 +1,7 @@
 package team18.team18_be.userInformation.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import team18.team18_be.userInformation.dto.request.CompanyRequest;
 import team18.team18_be.userInformation.dto.request.VisaRequest;
@@ -19,27 +14,27 @@ public class UserInformationController {
   private final UserInformationService userInformationService;
 
   public UserInformationController(UserInformationService userInformationService,
-    @LoginUser User user) {
+                                   @LoginUser User user) {
     this.userInformationService = userInformationService;
   }
 
   @PostMapping(value = "/sign", consumes = "multipart/form-data")
   public ResponseEntity<Void> fillInSign(@RequestParam("file") MultipartFile file,
-    @LoginUser User user) {
+                                         @LoginUser User user) {
     userInformationService.fillInSign(file);
     return ResponseEntity.noContent().build();
   }
 
   @PostMapping("/company")
   public ResponseEntity<Void> createCompany(@RequestBody CompanyRequest companyRequest,
-    @LoginUser User user) {
+                                            @LoginUser User user) {
     userInformationService.createCompany(companyRequest);
     return ResponseEntity.ok().build();
   }
 
   @PutMapping("/visa")
   public ResponseEntity<Void> fillInVisa(@RequestBody VisaRequest visaRequest,
-    @LoginUser User user) {
+                                         @LoginUser User user) {
     userInformationService.fillInVisa(visaRequest);
     return ResponseEntity.ok().build();
   }
