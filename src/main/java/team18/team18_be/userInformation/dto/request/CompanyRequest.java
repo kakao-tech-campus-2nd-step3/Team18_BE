bc30edd82d2b2343389b1c418025e8bd0498b0c5
test1 <test>
2024-09-26 18:03:50 +0900

    feat : record로 변경

--- src/main/java/team18/team18_be/userInformation/dto/request/CompanyRequest.java
+++ src/main/java/team18/team18_be/userInformation/dto/request/CompanyRequest.java
@@ -1,37 +1,4 @@
 package team18.team18_be.userInformation.dto.request;
 
-public class CompanyRequest {
-
-  private String company;
-  private String industryOccupation;
-  private String brand;
-  private String revenuePerYear;
-
-
-  public CompanyRequest() {
-  }
-
-  public CompanyRequest(String company, String industryOccupation, String brand,
-    String revenuePerYear) {
-    this.company = company;
-    this.industryOccupation = industryOccupation;
-    this.brand = brand;
-    this.revenuePerYear = revenuePerYear;
-  }
-
-  public String getCompany() {
-    return company;
-  }
-
-  public String getIndustryOccupation() {
-    return industryOccupation;
-  }
-
-  public String getBrand() {
-    return brand;
-  }
-
-  public String getRevenuePerYear() {
-    return revenuePerYear;
-  }
-}
+public record CompanyRequest(String company, String industryOccupation, String brand,
+                             String revenuePerYear) {}
