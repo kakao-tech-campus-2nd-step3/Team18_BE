d05283cbe39a84d67e81c222d85110367f36835a
test1 <test>
2024-10-09 01:47:40 +0900

    feat : erd에 따른 Company테이블의 field 변경

--- src/main/java/team18/team18_be/userInformation/entity/Company.java
+++ src/main/java/team18/team18_be/userInformation/entity/Company.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import team18.team18_be.auth.entity.User;
 
 @Table
 @Entity
@@ -15,21 +18,26 @@ public class Company {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private final Long companyId;
   @NotBlank
-  private final String companyName;
+  private String companyName;
   @NotBlank
-  private final String industryOccupation;
-  private final String brand;
+  private String industryOccupation;
+  private String brand;
   @NotBlank
-  private final Long revenuePerYear;
-  private final String logo;
+  private Long revenuePerYear;
+  private String logo;
+
+  @ManyToOne
+  @JoinColumn(name="user_id")
+  private User user;
 
   public Company(Long companyId, String companyName, String industryOccupation, String brand,
-      Long revenuePerYear, String logo, String hompageUrl) {
+      Long revenuePerYear, String logo, User user) {
     this.companyId = companyId;
     this.companyName = companyName;
     this.industryOccupation = industryOccupation;
     this.brand = brand;
     this.revenuePerYear = revenuePerYear;
     this.logo = logo;
+    this.user=user;
   }
 }
