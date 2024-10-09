07f9f5c41ec0997b48b0a3ad9276637f115c156b
test1 <test>
2024-10-09 20:42:48 +0900

    feat : visa정보 수정, 가져오기 api에 대한 controller, service 구현

--- src/main/java/team18/team18_be/userInformation/controller/UserInformationController.java
+++ src/main/java/team18/team18_be/userInformation/controller/UserInformationController.java
@@ -1,17 +1,21 @@
 package team18.team18_be.userInformation.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import team18.team18_be.auth.entity.User;
+import team18.team18_be.auth.service.AuthService;
 import team18.team18_be.config.resolver.LoginUser;
 import team18.team18_be.userInformation.dto.request.CompanyRequest;
 import team18.team18_be.userInformation.dto.request.VisaRequest;
+import team18.team18_be.userInformation.dto.request.VisaResponse;
 import team18.team18_be.userInformation.service.UserInformationService;
 
 @RestController
@@ -20,7 +24,8 @@ public class UserInformationController {
 
   private final UserInformationService userInformationService;
 
-  public UserInformationController(UserInformationService userInformationService) {
+  public UserInformationController(UserInformationService userInformationService,
+      AuthService authService) {
     this.userInformationService = userInformationService;
   }
 
@@ -32,7 +37,7 @@ public class UserInformationController {
   }
 
   @PostMapping("/company")
-  public ResponseEntity<Void> createCompany(@RequestBody CompanyRequest companyRequest,
+  public ResponseEntity<Void> createCompany(@RequestPart CompanyRequest companyRequest,
       @LoginUser User user) {
     userInformationService.createCompany(companyRequest);
     return ResponseEntity.ok().build();
@@ -41,7 +46,15 @@ public class UserInformationController {
   @PutMapping("/visa")
   public ResponseEntity<Void> fillInVisa(@RequestBody VisaRequest visaRequest,
       @LoginUser User user) {
-    userInformationService.fillInVisa(visaRequest);
+    userInformationService.fillInVisa(visaRequest, user);
     return ResponseEntity.ok().build();
   }
+
+  @GetMapping("/visa")
+  public ResponseEntity<VisaResponse> findVisa(@LoginUser User user) {
+    VisaResponse visaResponse = userInformationService.findVisa(user);
+    return ResponseEntity.ok(visaResponse);
+  }
+
+
 }
--- src/main/java/team18/team18_be/userInformation/service/UserInformationService.java
+++ src/main/java/team18/team18_be/userInformation/service/UserInformationService.java
@@ -2,18 +2,41 @@ package team18.team18_be.userInformation.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import team18.team18_be.auth.entity.User;
 import team18.team18_be.userInformation.dto.request.CompanyRequest;
 import team18.team18_be.userInformation.dto.request.VisaRequest;
+import team18.team18_be.userInformation.dto.request.VisaResponse;
+import team18.team18_be.userInformation.entity.Employee;
+import team18.team18_be.userInformation.repository.EmployeeRepository;
 
 @Service
 public class UserInformationService {
 
-  public void createCompany(CompanyRequest companyRequest) {
+  private final EmployeeRepository employeeRepository;
+
+  public UserInformationService(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
   }
 
-  public void fillInVisa(VisaRequest visaRequest) {
+  public void createCompany(CompanyRequest companyRequest) {
+    MultipartFile logoFile = companyRequest.logo();
+
+  }
+
+  public void fillInVisa(VisaRequest visaRequest, User user) {
+    Employee employee = employeeRepository.findByUser(user);
+    Employee newEmployee = new Employee(employee.getId(), visaRequest.foreginerNumber(),
+        visaRequest.visaGenereteDate(), user);
+    employeeRepository.save(newEmployee);
   }
 
   public void fillInSign(MultipartFile file) {
+  }
+
+  public VisaResponse findVisa(User user) {
+    Employee employee = employeeRepository.findByUser(user);
+    VisaResponse visaResponse = new VisaResponse(employee.getForeginerNumber(),
+        employee.getVisaGenerateDate());
+    return visaResponse;
   }
 }
