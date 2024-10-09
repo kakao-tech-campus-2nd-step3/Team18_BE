e93260a4199f97604dbe2bc32a435ee9be2d6be4
test1 <test>
2024-10-09 19:47:10 +0900

    feat : 외국인근로자(employee)엔티티 생성

---
+++ src/main/java/team18/team18_be/userInformation/entity/Employee.java
@@ -0,0 +1,29 @@
+package team18.team18_be.userInformation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import team18.team18_be.auth.entity.User;
+
+@Table
+@Entity
+public class Employee {
+  @Id
+  private final Long id;
+  private String foreginerNumber;
+  private LocalDateTime visaGenerateDate;
+  @OneToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  public Employee(Long id, String foreginerNumber, LocalDateTime visaGenerateDate, User user) {
+    this.id = id;
+    this.foreginerNumber = foreginerNumber;
+    this.visaGenerateDate = visaGenerateDate;
+    this.user = user;
+  }
+}
