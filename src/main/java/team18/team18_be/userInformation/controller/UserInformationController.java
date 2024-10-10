b19e30eef55cfa3ac84a7913ffade7bda9050609
test1 <test>
2024-10-10 23:14:11 +0900

    feat(test용) : 회사 등록 및 가져오기, 사인 등록 및 가져오기, 비자 등록 및 가져오기 구현

--- src/main/java/team18/team18_be/userInformation/controller/UserInformationController.java
+++ src/main/java/team18/team18_be/userInformation/controller/UserInformationController.java
@@ -14,6 +14,8 @@ import team18.team18_be.auth.entity.User;
 import team18.team18_be.auth.service.AuthService;
 import team18.team18_be.config.resolver.LoginUser;
 import team18.team18_be.userInformation.dto.request.CompanyRequest;
+import team18.team18_be.userInformation.dto.request.CompanyResponse;
+import team18.team18_be.userInformation.dto.request.SignResponse;
 import team18.team18_be.userInformation.dto.request.VisaRequest;
 import team18.team18_be.userInformation.dto.request.VisaResponse;
 import team18.team18_be.userInformation.service.UserInformationService;
@@ -29,32 +31,65 @@ public class UserInformationController {
     this.userInformationService = userInformationService;
   }
 
-  @PostMapping(value = "/sign", consumes = "multipart/form-data")
-  public ResponseEntity<Void> fillInSign(@RequestParam("file") MultipartFile file,
-      @LoginUser User user) {
-    userInformationService.fillInSign(file);
+  //  @PostMapping(value = "/sign", consumes = "multipart/form-data")
+//  public ResponseEntity<Void> fillInSign(@RequestParam("file") MultipartFile file,
+//      @LoginUser User user) {
+//    userInformationService.fillInSign(file);
+//    return ResponseEntity.noContent().build();
+//  }
+  @PostMapping("/sign")
+  public ResponseEntity<Void> fillInSign(@RequestParam MultipartFile signImg) {
+    userInformationService.fillInSign(signImg);
     return ResponseEntity.noContent().build();
   }
 
+//  @GetMapping(value="/sign")
+//  public ResponseEntity<SignResponse> findSign(@LoginUser User user){
+//    return ResponseEntity.ok(userInformationService.findSign(user));
+//  }
+  @GetMapping(value="/sign")
+  public ResponseEntity<SignResponse> findSign(){
+    return ResponseEntity.ok(userInformationService.findSign());
+  }
+
+  //  @PostMapping("/company")
+//  public ResponseEntity<Void> createCompany(@RequestPart CompanyRequest companyRequest,
+//      @RequestPart MultipartFile multipartFile, @LoginUser User user) {
+//    userInformationService.createCompany(companyRequest,multipartFile);
+//    return ResponseEntity.ok().build();
+//  }
   @PostMapping("/company")
   public ResponseEntity<Void> createCompany(@RequestPart CompanyRequest companyRequest,
-      @LoginUser User user) {
-    userInformationService.createCompany(companyRequest);
+      @RequestPart MultipartFile logo) {
+    userInformationService.createCompany(companyRequest, logo);
     return ResponseEntity.ok().build();
   }
 
+  @GetMapping("/company")
+  public ResponseEntity<CompanyResponse> findCompany(){
+    return ResponseEntity.ok(userInformationService.findCompany());
+  }
+
+//  @PutMapping("/visa")
+//  public ResponseEntity<Void> fillInVisa(@RequestBody VisaRequest visaRequest,
+//      @LoginUser User user) {
+//    userInformationService.fillInVisa(visaRequest, user);
+//    return ResponseEntity.ok().build();
+//  }
   @PutMapping("/visa")
-  public ResponseEntity<Void> fillInVisa(@RequestBody VisaRequest visaRequest,
-      @LoginUser User user) {
-    userInformationService.fillInVisa(visaRequest, user);
+  public ResponseEntity<Void> fillInVisa(@RequestBody VisaRequest visaRequest) {
+    userInformationService.fillInVisa(visaRequest);
     return ResponseEntity.ok().build();
   }
 
+//  @GetMapping("/visa")
+//  public ResponseEntity<VisaResponse> findVisa(@LoginUser User user) {
+//    VisaResponse visaResponse = userInformationService.findVisa(user);
+//    return ResponseEntity.ok(visaResponse);
+//  }
   @GetMapping("/visa")
-  public ResponseEntity<VisaResponse> findVisa(@LoginUser User user) {
-    VisaResponse visaResponse = userInformationService.findVisa(user);
+  public ResponseEntity<VisaResponse> findVisa() {
+    VisaResponse visaResponse = userInformationService.findVisa();
     return ResponseEntity.ok(visaResponse);
   }
-
-
 }
--- src/main/java/team18/team18_be/userInformation/service/UserInformationService.java
+++ src/main/java/team18/team18_be/userInformation/service/UserInformationService.java
@@ -1,42 +1,105 @@
 package team18.team18_be.userInformation.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import team18.team18_be.auth.entity.User;
+import team18.team18_be.config.GCS.GcsUploader;
 import team18.team18_be.userInformation.dto.request.CompanyRequest;
+import team18.team18_be.userInformation.dto.request.CompanyResponse;
+import team18.team18_be.userInformation.dto.request.SignResponse;
 import team18.team18_be.userInformation.dto.request.VisaRequest;
 import team18.team18_be.userInformation.dto.request.VisaResponse;
+import team18.team18_be.userInformation.entity.Company;
 import team18.team18_be.userInformation.entity.Employee;
+import team18.team18_be.userInformation.entity.Sign;
+import team18.team18_be.userInformation.repository.CompanyRepository;
 import team18.team18_be.userInformation.repository.EmployeeRepository;
+import team18.team18_be.userInformation.repository.SignRepository;
 
 @Service
 public class UserInformationService {
 
   private final EmployeeRepository employeeRepository;
+  private final CompanyRepository companyRepository;
+  private final SignRepository signRepository;
+  private final GcsUploader gcsUploader;
 
-  public UserInformationService(EmployeeRepository employeeRepository) {
+
+  public UserInformationService(EmployeeRepository employeeRepository,
+      CompanyRepository companyRepository, SignRepository signRepository, GcsUploader gcsUploader) {
     this.employeeRepository = employeeRepository;
+    this.companyRepository = companyRepository;
+    this.signRepository = signRepository;
+    this.gcsUploader=gcsUploader;
   }
 
-  public void createCompany(CompanyRequest companyRequest) {
-    MultipartFile logoFile = companyRequest.logo();
-
+  public void createCompany(CompanyRequest companyRequest, MultipartFile logo) {
+    String storedFileName = gcsUploader.upload(logo, "companyLogo")
+        .orElseThrow(() -> new NoSuchElementException("파일 업로드에 실패했습니다."));
+    Company company = new Company(companyRequest.company(), companyRequest.industryOccupation(),
+        companyRequest.brand(), companyRequest.revenuePerYear(), storedFileName);
+    companyRepository.save(company);
   }
 
-  public void fillInVisa(VisaRequest visaRequest, User user) {
-    Employee employee = employeeRepository.findByUser(user);
-    Employee newEmployee = new Employee(employee.getId(), visaRequest.foreginerNumber(),
-        visaRequest.visaGenereteDate(), user);
+  public CompanyResponse findCompany() {
+    Company company = companyRepository.findById(Long.valueOf(1))
+        .orElseThrow(() -> new NoSuchElementException("해당 companydData가 없습니다."));
+    CompanyResponse companyResponse = new CompanyResponse(company.getCompanyName(),
+        company.getIndustryOccupation(), company.getBrand(), company.getRevenuePerYear(),
+        company.getLogo());
+    return companyResponse;
+  }
+
+  //  public void fillInVisa(VisaRequest visaRequest, User user) {
+//    Employee employee = employeeRepository.findByUser(user);
+//    Employee newEmployee = new Employee(employee.getId(), visaRequest.foreginerNumber(),
+//        visaRequest.visaGenereteDate(), user);
+//    employeeRepository.save(newEmployee);
+//  }
+  public void fillInVisa(VisaRequest visaRequest) {
+    Employee newEmployee = new Employee(visaRequest.foreginerNumber(),
+        visaRequest.visaGenereteDate());
     employeeRepository.save(newEmployee);
   }
 
-  public void fillInSign(MultipartFile file) {
-  }
-
-  public VisaResponse findVisa(User user) {
-    Employee employee = employeeRepository.findByUser(user);
+  //  public VisaResponse findVisa(User user) {
+//    Employee employee = employeeRepository.findByUser(user);
+//    VisaResponse visaResponse = new VisaResponse(employee.getForeginerNumber(),
+//        employee.getVisaGenerateDate());
+//    return visaResponse;
+//  }
+  public VisaResponse findVisa() {
+    Employee employee = employeeRepository.findById(Long.valueOf(1))
+        .orElseThrow(() -> new NoSuchElementException("해당 employee가 없습니다."));
     VisaResponse visaResponse = new VisaResponse(employee.getForeginerNumber(),
         employee.getVisaGenerateDate());
     return visaResponse;
   }
+
+
+  public void fillInSign(MultipartFile signImg) {
+    String storedFileName = gcsUploader.upload(signImg, "Sign")
+        .orElseThrow(() -> new NoSuchElementException("파일 업로드에 실패했습니다."));
+    Sign newSign = new Sign(storedFileName);
+    signRepository.save(newSign);
+  }
+
+  //  public SignResponse findSign(User user) {
+  //유저로 사인 엔티티 갖고오고 사인 이미지url 리턴하기
+//    Sign sign = signRepository.findByUser(user);
+//    SignResponse signResponse = new SignResponse(sign.getImageUrl());
+//    return signResponse;
+//  }
+  public SignResponse findSign() {
+    List<Sign> signList = signRepository.findAll();
+    String init = "";
+    for (Sign sign : signList) {
+      init = sign.getImageUrl();
+    }
+    SignResponse signResponse = new SignResponse(init);
+    return signResponse;
+  }
+
+
 }
