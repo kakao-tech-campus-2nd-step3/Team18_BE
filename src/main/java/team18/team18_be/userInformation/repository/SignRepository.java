f1fa40c9a1cf28698969e5cedf72140e664a5e48
test1 <test>
2024-10-10 23:12:35 +0900

    feat : company, sign, employee repository 함수 생성

--- src/main/java/team18/team18_be/userInformation/repository/SignRepository.java
+++ src/main/java/team18/team18_be/userInformation/repository/SignRepository.java
@@ -1,8 +1,9 @@
 package team18.team18_be.userInformation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team18.team18_be.auth.entity.User;
 import team18.team18_be.userInformation.entity.Sign;
 
 public interface SignRepository extends JpaRepository<Sign, Long> {
-
+  Sign findByUser(User user);
 }
