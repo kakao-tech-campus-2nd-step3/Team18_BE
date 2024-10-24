5edf10885df33280d72fb8e8fa8c41a048cdadc9
test1 <test>
2024-10-25 04:38:23 +0900

    feat : 필수정보 DTO 생성

---
+++ src/main/java/team18/team18_be/apply/dto/response/MandatoryResponse.java
@@ -0,0 +1,6 @@
+package team18.team18_be.apply.dto.response;
+
+public record MandatoryResponse(boolean resumeExistence, boolean visaExistence,
+                                boolean foreignerIdNumberExistence) {
+
+}
